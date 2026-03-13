0963b42a13cbd1966bfa0f483861c1ef8ca1f359
JoHaggvi <jonatan.haggvik@abo.fi>
2026-03-13 17:46:17 +0200

    test

--- content.js
+++ content.js
@@ -1,96 +1,40 @@
-function applyBoldFormatting() {
-  var textElements = document.querySelectorAll("p,  td");
-
-  for (var i = 0; i < textElements.length; i++) {
-    var words = textElements[i].childNodes;
-
-    var formattedWords = [];
-
-    for (var j = 0; j < words.length; j++) {
-      var word = words[j];
-
-      if (word.nodeName === "#text") {
-        var text = word.textContent;
-        var formattedText = formatText(text);
-        var span = document.createElement("span");
-        span.innerHTML = formattedText;
-        textElements[i].replaceChild(span, word);
-        formattedWords.push(span);
-      } else if (word.nodeName === "SPAN") {
-        var text = word.textContent;
-        var formattedText = formatText(text);
-        var span = document.createElement("span");
-        span.innerHTML = formattedText;
-        formattedWords.push(span);
-      } else {
-        formattedWords.push(word);
-      }
-    }
-
-    textElements[i].innerHTML = "";
-    formattedWords.forEach(function (word) {
-      textElements[i].appendChild(word);
-    });
-  }
-}
-
 function formatText(text) {
-  var words = text.split(" ");
-  var formattedWords = words.map(function (word) {
-    if (word.includes("class") && word.includes("=")) {
-      var classText = extractClassText(word);
-      return word.replace(classText, formatClassText(classText));
-    }
+  const words = text.split(" ");
+  return words.map(word => {
+    if (word.length < 2) return word;
+    return "<b>" + word.substr(0, 2) + "</b>" + word.substr(2);
+  }).join(" ");
+}
 
-    if (word.includes("<a") && word.includes("href")) {
-      var linkText = extractLinkText(word);
-      return word.replace(linkText, formatLinkText(linkText));
-    }
+function processNode(el) {
+  if (el.dataset && el.dataset.boldFormatted) return;
+  if (el.dataset) el.dataset.boldFormatted = "true";
 
-    if (word.length < 2) {
-      return word;
-    }
+  const walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null);
+  const textNodes = [];
+  let node;
+  while ((node = walker.nextNode())) textNodes.push(node);
 
-    var firstTwoLetters = "<b>" + word.substr(0, 2) + "</b>";
-    var remainingLetters = word.substr(2);
-    return firstTwoLetters + remainingLetters;
+  textNodes.forEach(textNode => {
+    const text = textNode.textContent;
+    if (!text.trim()) return;
+
+    const span = document.createElement("span");
+    span.innerHTML = formatText(text);
+    textNode.parentNode.replaceChild(span, textNode);
   });
-
-  return formattedWords.join(" ");
 }
 
-function extractClassText(word) {
-  var startIndex = word.indexOf("=") + 1;
-  var endIndex = word.length - 1;
-  var classText = word.substring(startIndex, endIndex);
-  return classText;
+function applyBoldFormatting() {
+  document.querySelectorAll("p, td, h1, h2, h3, li").forEach(processNode);
 }
 
-function formatClassText(classText) {
-  var formattedText = classText.replace(/(\w{2})/g, "<b>$1</b>");
-  return formattedText;
-}
-
-function extractLinkText(word) {
-  var startIndex = word.indexOf(">") + 1;
-  var endIndex = word.lastIndexOf("</a>");
-  var linkText = word.substring(startIndex, endIndex);
-  return linkText;
-}
-
-function formatLinkText(linkText) {
-  var words = linkText.split(" ");
-  var formattedWords = words.map(function (word) {
-    if (word.length < 2) {
-      return word;
-    }
-    var firstTwoLetters = "<b>" + word.substr(0, 2) + "</b>";
-    var remainingLetters = word.substr(2);
-    return firstTwoLetters + remainingLetters;
-  });
-
-  return formattedWords.join(" ");
-}
-
-
+// Run once on load
 applyBoldFormatting();
+
+// Watch for dynamic content
+const observer = new MutationObserver(() => {
+  applyBoldFormatting();
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
